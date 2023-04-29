76841e7cd9c8ea4fd3a774547d2ea07463c3e54d
Chris Holst <chris@seam.es>
2023-04-29 14:22:10 +0200

    attempt nr something

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,24 @@ use std::path::PathBuf;
 
 mod api;
 mod models;
-
-async fn index() -> impl Responder {
-    NamedFile::open_async("./static/index.html")
+/*
+async fn index(static_folder: PathBuf) -> impl Responder {
+    NamedFile::open_async(static_folder"./static/index.html")
         .await
         .map_err(|e| actix_web::error::ErrorInternalServerError(e))
 }
+ */
 
 #[shuttle_runtime::main]
 async fn actix_web(
     #[shuttle_static_folder::StaticFolder] static_folder: PathBuf,
 ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+    let static_folder = static_folder.join("index.html");
+
+    let index = NamedFile::open_async(static_folder)
+        .await
+        .map_err(|e| actix_web::error::ErrorInternalServerError(e))
+    
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(web::resource("/").route(web::get().to(index)));
         cfg.service(api::openmeteo::openmeteo);

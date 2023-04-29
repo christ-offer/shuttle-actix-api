e45d5bec50448b3d17f07bdb20e366a232d2b678
Chris Holst <chris@seam.es>
2023-04-29 13:00:53 +0200

    attempt at adding in website static serving also

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use actix_web::{get, web::ServiceConfig};
+use actix_files::NamedFile;
+//use actix_web::{get, web::ServiceConfig};
+use actix_web::{
+    web::{self, ServiceConfig},
+    HttpRequest, HttpResponse, Responder,
+};
 use shuttle_actix_web::ShuttleActixWeb;
 
 mod api;
@@ -9,10 +14,17 @@ async fn hello_world() -> &'static str {
     "Hello World!"
 }
 
+async fn index() -> impl Responder {
+    NamedFile::open_async("./static/index.html")
+        .await
+        .map_err(|e| actix_web::error::ErrorInternalServerError(e))
+}
+
 #[shuttle_runtime::main]
 async fn actix_web(
 ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
+        cfg.service(web::resource("/").route(web::get().to(index)))
         cfg.service(api::openmeteo::openmeteo);
         cfg.service(hello_world);
     };
